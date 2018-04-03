f45e609570f4e30f03c74d64e36ee0e0f2b7d422
warlordlizard <alexjbowers88@gmail.com>
2018-04-03 13:51:59 -0700

    structured folders and added lib items

---
+++ lab-alex/frontend/src/lib/redux-reporter.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const reporter = store => next => action => {
+  console.log('__ACTION IN REDUX REPORTER__', action);
+  try {
+    let result = next(action);
+    console.log('__STATE IN REDUX REPORTER', store.getState());
+    return result;
+  } catch (err) {
+    err.action = action;
+    console.error('__ERROR__', err);
+    return err;
+  }
+}
+
+export default reporter;
