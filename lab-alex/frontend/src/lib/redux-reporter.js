1160eac45b3ee668ba2e6444b2d2b8c6959363b6
warlordlizard <alexjbowers88@gmail.com>
2018-04-03 15:38:59 -0700

    forgot __ after state

--- lab-alex/frontend/src/lib/redux-reporter.js
+++ lab-alex/frontend/src/lib/redux-reporter.js
@@ -4,7 +4,7 @@ const reporter = store => next => action => {
   console.log('__ACTION IN REDUX REPORTER__', action);
   try {
     let result = next(action);
-    console.log('__STATE IN REDUX REPORTER', store.getState());
+    console.log('__STATE IN REDUX REPORTER__', store.getState());
     return result;
   } catch (err) {
     err.action = action;
