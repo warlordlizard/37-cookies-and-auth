668b5aade35c86b1de4ea7f03ebcb8c838255421
warlordlizard <alexjbowers88@gmail.com>
2018-04-04 16:00:38 -0700

    settings and pofile-form components done

---
+++ lab-alex/frontend/src/components/settings/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import ProfileForm from '../profile-form';
+import {profileCreateRequest} from '../../actions/profile-actions.js';
+
+class SettingS extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleProfileCreate = this.handleProfileCreate.bind(this);
+  }
+  handleProfileCreate(profile) {
+    return this.props.profileCreate(profile)
+      .then( res => {
+        console.log('profile created:', res);
+      })
+      .catch(console.error);
+  }
+
+  render() {
+    let handleComplete = this.props.profile ? this.handleProfileCreate : null;
+
+    return(
+      <section className='settings'>
+        <h2>Settings:</h2>
+        <ProfileForm
+          buttonText='create profile'
+          onComplet={this.handleProfileCreate} />
+      </section>
+    );
+  }
+}
+
+let mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
+let mapDispatchToProps = (dispatch) => ({
+  profileCreate: (profile) => dispatch(profileCreateRequest(profile)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings);
