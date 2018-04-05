444d6ce1073ff569574f4c3bc43e3d9806ee88c2
warlordlizard <alexjbowers88@gmail.com>
2018-04-05 13:50:01 -0700

    new packages added and added logout action

--- lab-alex/frontend/src/actions/auth-actions.js
+++ lab-alex/frontend/src/actions/auth-actions.js
@@ -1,13 +1,15 @@
 import superagent from 'superagent';
+import * as util from '../lib/util.js';
 
 export const tokenSet = (token) => ({
   type: 'TOKEN_SET',
   payload: token,
 });
 
-export const tokenDelete = () => ({
-  type: 'TOKEN_DELETE',
-});
+export const logout = () => {
+  util.deleteCookie('X-Sluggram-Token');
+  return { type: 'LOGOUT' };
+};
 
 export const signupRequest = (user) => (dispatch) => {
   return superagent.post(`${__API_URL__}/signup`)
