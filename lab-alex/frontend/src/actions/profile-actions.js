14053103e57ec32b195a161e1d369847e4705c35
warlordlizard <alexjbowers88@gmail.com>
2018-04-05 13:54:05 -0700

    profile actions

--- lab-alex/frontend/src/actions/profile-actions.js
+++ lab-alex/frontend/src/actions/profile-actions.js
@@ -21,3 +21,25 @@ export const profileCreateRequest = (profile) => (dispatch, getState) => {
       return res;
     });
 };
+
+export const profileUpdateRequest = (profile) => (dispatch, getState) => {
+  let { auth } = getState();
+  return superagent.put(`${__API_URL__}/profiles/${profile._id}`)
+    .set('Authorization', `Bearer ${auth}`)
+    .field('bio', profile.bio)
+    .attach('avatar', profile.avatar)
+    .then(res => {
+      dispatch(profileCreate(res.body));
+      return res;
+    });
+};
+
+export const profileFetchRequest = (profile) => (dispatch, getState) => {
+  let { auth } = getState();
+  return superagent.get(`${__API_URL__}/profiles/me`)
+    .set('Authorization', `Bearer ${auth}`)
+    .then(res => {
+      dispatch(profileCreate(res.body));
+      return res;
+    });
+};
